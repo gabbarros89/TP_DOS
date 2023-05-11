4052515ea639ded0c0610ab2d9767b5ccb191fdb
gabriel <abarros@fi.uba.ar>
2023-05-11 18:59:28 -0300

    Creando los nodos

---
+++ NodoLectura.h
@@ -0,0 +1,23 @@
+//
+// Created by gabriel on 11/05/23.
+//
+
+#ifndef TP_DOS_NODOLECTURA_H
+#define TP_DOS_NODOLECTURA_H
+#include <iostream>
+
+
+using namespace std;
+
+
+
+class NodoLectura {
+protected:
+    string dato;
+
+
+
+};
+
+
+#endif //TP_DOS_NODOLECTURA_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include "NodoLectura.h"
+
+using namespace std;
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
