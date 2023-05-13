2897d783d3da36463357ed7faede1981d4fd39dc
gabriel <abarros@fi.uba.ar>
2023-05-13 08:51:23 -0300

    implementando vector como nodo

--- Vector.h
+++ Vector.h
@@ -5,44 +5,6 @@
 #ifndef TP_DOS_VECTOR_H
 #define TP_DOS_VECTOR_H
 
-template <typename generico> class Vector {
-private:
-    int tamanio, cantidad;
-    generico * elementos;
-
-public:
-
-    //PRE:
-    //POS: crea un vector vacio
-    Vector();
-
-    //PRE:
-    //POS:
-    void pushback(generico algo);
-
-    //PRE:
-    //POS:
-    void expandirVector();
-
-    //PRE:
-    //POS:
-    void reducirVector()
-
-
-    //PRE:
-    //POS:
-    generico popBack();
-
-    //PRE:
-    //POS:
-    generico &elemento(int indice);
-
-
-    //Destructor
-    //PRE:
-    //POS:
-    ~Vector();
-};
 
 
 #endif //TP_DOS_VECTOR_H
